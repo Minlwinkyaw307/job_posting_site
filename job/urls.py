b018670bfce1307029e831585a76747a4c93f3a7
Min Lwin <minlwinkyaw307@gmail.com>
2020-03-30 07:10:20 +0300

    Index Page Created

--- job/urls.py
+++ job/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('test/', test),
+    path('', index, name='job.index'),
 ]
 
--- job/views.py
+++ job/views.py
@@ -2,8 +2,23 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import *
 
+from .models import *
+from setting.models import *
 
-# Create your views here.
+
+def index(request):
+    # getting first setting
+    setting = Setting.objects.all()[0]
+    jobs = Job.objects.filter(status__id=1)
+    print(jobs[0].image)
+    data = {
+        'title': "Index Title",
+        'setting': setting,
+        'jobs': jobs,
+    }
+    return render(request, 'job/index.html', data)
+
+
 def test(request):
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
